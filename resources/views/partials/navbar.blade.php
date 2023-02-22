a50a3e570d18af3980341baa01be02525b3623db
Willyam <willyamwilly914@gmail.com>
2023-02-22 11:29:59 +0700

    second-commit

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -81,7 +81,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-danger py-3">
     <div class="container">
-        <a class="navbar-brand fw-bold" href="/"><i class="bi bi-megaphone"></i> Pengaduan Sekolah</a>
+        <a class="navbar-brand fw-bold" href="/"><i class="bi bi-megaphone"></i> Pengaduan Siswa</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
